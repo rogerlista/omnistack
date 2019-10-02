cb3b1dd0bb2053f99ddf46ee6b36841d3e0df8c9
Rogério <roger.lista@gmail.com>
2019-10-02 06:48:25 -0300

    Aircnc - configuração do mongodb

--- aircnc/backend/src/server.js
+++ aircnc/backend/src/server.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const routes = require('./routes')
 
 const app = express()
+
+mongoose.connect('mongodb://localhost:27017/semana09', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
 app.use(express.json())
 app.use(routes)
