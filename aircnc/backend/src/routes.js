7c8e28f71cd0657fcbaf7b7a0626c6bb4ea35135
Rogério <roger.lista@gmail.com>
2019-10-02 07:23:17 -0300

    Aircnc - login do usuário

--- aircnc/backend/src/routes.js
+++ aircnc/backend/src/routes.js
@@ -2,14 +2,14 @@ const express = require('express')
 
 const routes = express.Router()
 
+const sessionController = require('./controllers/session-controller')
+
 // GET, POST, PUT, DELETE
 
 // req.query = Acessar query params (para filtros)
 // req.params = Acessar route params (para edição e delete)
 // req.body = Acessar corpo da requisição (para criação, edição)
 
-routes.post('/users', (req, res) => {
-  return res.json(req.body)
-})
+routes.post('/sessions', sessionController.store)
 
 module.exports = routes
