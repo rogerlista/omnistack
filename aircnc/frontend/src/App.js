6668f2ad90195dc99e63ff3ffa67c35d5f5295f0
Rogério <roger.lista@gmail.com>
2019-10-03 06:43:00 -0300

    Aircnc - consumindo a api do backend com axios

--- aircnc/frontend/src/App.js
+++ aircnc/frontend/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 import logo from './assets/logo.svg'
 
+import api from './services/api'
+
 function App() {
+  const [email, setEmail] = useState('')
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+
+    const response = await api.post('/sessions', { email })
+
+    const { _id } = response.data
+    localStorage.setItem('user', _id)
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="AirCnC" />
@@ -14,9 +27,15 @@ function App() {
           <strong>talentos</strong> para sua empresa
         </p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-MAIL *</label>
-          <input id="email" type="email" placeholder="Seu melhor e-mail" />
+          <input
+            id="email"
+            type="email"
+            placeholder="Seu melhor e-mail"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
 
           <button type="submit" className="btn">
             Entrar
