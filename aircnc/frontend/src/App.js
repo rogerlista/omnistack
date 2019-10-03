ae62ecddeaaef2527b22f71143ff0bed44b90c04
Rogério <roger.lista@gmail.com>
2019-10-03 05:55:56 -0300

    Aircnc - tela de login

--- aircnc/frontend/src/App.js
+++ aircnc/frontend/src/App.js
@@ -1,8 +1,30 @@
 import React from 'react'
 import './App.css'
 
+import logo from './assets/logo.svg'
+
 function App() {
-  return <h1>Hello AirCnC</h1>
+  return (
+    <div className="container">
+      <img src={logo} alt="AirCnC" />
+
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre{' '}
+          <strong>talentos</strong> para sua empresa
+        </p>
+
+        <form>
+          <label htmlFor="email">E-MAIL *</label>
+          <input id="email" type="email" placeholder="Seu melhor e-mail" />
+
+          <button type="submit" className="btn">
+            Entrar
+          </button>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default App
