c2cd1928781963c20929125ef0ad2de59727a99d
Rogério <roger.lista@gmail.com>
2019-10-04 06:28:33 -0300

    Aircnc - iniciando o projeto mobile

---
+++ aircnc/mobile/App.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello OmniStack</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
