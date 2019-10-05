36d493d78d4873f1e5f8f8f8bc78903632640edc
Rogério <roger.lista@gmail.com>
2019-10-05 07:58:56 -0300

    Aircnc - tela Book

--- aircnc/mobile/src/pages/book.js
+++ aircnc/mobile/src/pages/book.js
@@ -1,12 +1,86 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  AsyncStorage,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 
 export default function Book({navigation}) {
+  const [date, setDate] = useState('');
   const id = navigation.getParam('id');
 
+  function handleSubmit() {}
+
+  function handleCancel() {}
+
   return (
-    <SafeAreaView>
-      <Text>{id}</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.label}>DATA DE INTERESSE *</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Qual data você quer reservar?"
+        placeholderTextColor="#999"
+        autoCapitalize="words"
+        autoCorrect={false}
+        value={date}
+        onChangeText={setDate}
+      />
+
+      <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+        <Text style={styles.buttonText}>Solicitar reserva</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={handleCancel}
+        style={[styles.button, styles.cancelButton]}>
+        <Text style={styles.buttonText}>Cancelar</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 30,
+  },
+
+  label: {
+    fontWeight: 'bold',
+    color: '#444',
+    marginBottom: 8,
+    marginTop: 30,
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    paddingHorizontal: 20,
+    fontSize: 16,
+    color: '#444',
+    height: 44,
+    marginBottom: 20,
+    borderRadius: 2,
+  },
+
+  button: {
+    height: 42,
+    backgroundColor: '#f05a5b',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 2,
+  },
+
+  cancelButton: {
+    backgroundColor: '#ccc',
+    marginTop: 10,
+  },
+
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
