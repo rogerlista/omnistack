9ea49e20440b0da70dea4f8c7c8de7ee5bd1876d
Rogério <roger.lista@gmail.com>
2020-01-14 23:34:23 -0300

    Rota de busca de todos os devs

--- radar-dev/backend/src/controllers/dev-controller.js
+++ radar-dev/backend/src/controllers/dev-controller.js
@@ -1,31 +1,43 @@
 const axios = require("axios");
 const Dev = require("../models/dev");
 
+// index. show, store, update, destroy
+
 module.exports = {
+  async index(request, response) {
+    const devs = await Dev.find();
+
+    return response.json(devs);
+  },
+
   async store(request, response) {
     const { github_username, techs, latitude, longitude } = request.body;
 
-    const apiResponse = await axios.get(
-      `https://api.github.com/users/${github_username}`
-    );
+    let dev = await Dev.findOne({ github_username });
 
-    const { name = login, avatar_url, bio } = apiResponse.data;
+    if (!dev) {
+      const apiResponse = await axios.get(
+        `https://api.github.com/users/${github_username}`
+      );
 
-    const techsArray = techs.split(",").map(tech => tech.trim());
+      const { name = login, avatar_url, bio } = apiResponse.data;
 
-    const location = {
-      type: "Point",
-      coordinates: [longitude, latitude]
-    };
+      const techsArray = techs.split(",").map(tech => tech.trim());
 
-    const dev = await Dev.create({
-      github_username,
-      name,
-      avatar_url,
-      bio,
-      techs: techsArray,
-      location
-    });
+      const location = {
+        type: "Point",
+        coordinates: [longitude, latitude]
+      };
+
+      dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location
+      });
+    }
 
     return response.json(dev);
   }
--- radar-dev/backend/src/routes.js
+++ radar-dev/backend/src/routes.js
@@ -3,6 +3,7 @@ const devController = require("./controllers/dev-controller");
 
 const routes = Router();
 
+routes.get("/devs", devController.index);
 routes.post("/devs", devController.store);
 
 module.exports = routes;
