c3a6cefcdfb0a75e1990c77ed65693382d23509f
Rogério <roger.lista@gmail.com>
2020-01-15 20:33:41 -0300

    Página de cadastro do usuário e listagem

--- radar-dev/web/src/App.js
+++ radar-dev/web/src/App.js
@@ -1,24 +1,125 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./global.css";
+import "./app.css";
+import "./sidebar.css";
+import "./main.css";
+
+// Três conceitos principais do react
+
+// Componente: bloco isolado de HTML, CSS e JS o qual não interfere no restante da aplicação.
+// Propriedade: informações que um componente PAI passa para o componente FILHO.
+// Estado: informações mantidas pelo componente (lembrar: imutabilidade).
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastar</strong>
+
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/300170?s=400&v=4"
+                alt="Rogério Gomes Rios"
+              />
+              <div className="user-info">
+                <strong>Rogério Gomes Rios</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+
+            <p>
+              CTO na @RogerLista. Apaixonado pelas melhores tecnologias de
+              desenvolvimento
+            </p>
+            <a href="http://github.com/rogerlista">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/300170?s=400&v=4"
+                alt="Rogério Gomes Rios"
+              />
+              <div className="user-info">
+                <strong>Rogério Gomes Rios</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+
+            <p>
+              CTO na @RogerLista. Apaixonado pelas melhores tecnologias de
+              desenvolvimento
+            </p>
+            <a href="http://github.com/rogerlista">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/300170?s=400&v=4"
+                alt="Rogério Gomes Rios"
+              />
+              <div className="user-info">
+                <strong>Rogério Gomes Rios</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+
+            <p>
+              CTO na @RogerLista. Apaixonado pelas melhores tecnologias de
+              desenvolvimento
+            </p>
+            <a href="http://github.com/rogerlista">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/300170?s=400&v=4"
+                alt="Rogério Gomes Rios"
+              />
+              <div className="user-info">
+                <strong>Rogério Gomes Rios</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+
+            <p>
+              CTO na @RogerLista. Apaixonado pelas melhores tecnologias de
+              desenvolvimento
+            </p>
+            <a href="http://github.com/rogerlista">Acessar perfil no Github</a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
