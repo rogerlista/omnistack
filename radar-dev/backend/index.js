051dba7ca7271e18bde8b837f0c9db8d53059d39
Rogério <roger.lista@gmail.com>
2020-01-14 22:34:20 -0300

    Configuração inicial do mongodb

--- radar-dev/backend/index.js
+++ radar-dev/backend/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect(
+  "mongodb+srv://juca:minhasenha@cluster0-k6gcz.mongodb.net/week10?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
 app.use(express.json());
 
