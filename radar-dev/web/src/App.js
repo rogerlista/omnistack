adea450eb01b492a4e19faad40c735e3c84957dc
Rogério <roger.lista@gmail.com>
2020-01-15 21:16:01 -0300

    Cadastando usuarios e listando

--- radar-dev/web/src/App.js
+++ radar-dev/web/src/App.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from "./services/api";
 
 import "./global.css";
 import "./app.css";
@@ -12,31 +14,108 @@ import "./main.css";
 // Estado: informações mantidas pelo componente (lembrar: imutabilidade).
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  const [github_username, setGithubUsername] = useState("");
+  const [techs, setTechs] = useState("");
+
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      err => {
+        console.log(err);
+      },
+      {
+        timeout: 30000
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get("/devs");
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(e) {
+    e.preventDefault();
+
+    const response = await api.post("/devs", {
+      github_username,
+      techs,
+      latitude,
+      longitude
+    });
+
+    setGithubUsername("");
+    setTechs("");
+
+    setDevs([...devs, response.data]);
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastar</strong>
 
-        <form>
+        <form onSubmit={handleAddDev}>
           <div className="input-block">
             <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required />
+            <input
+              name="github_username"
+              id="github_username"
+              required
+              value={github_username}
+              onChange={e => setGithubUsername(e.target.value)}
+            />
           </div>
 
           <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
+            <input
+              name="techs"
+              id="techs"
+              required
+              value={techs}
+              onChange={e => setTechs(e.target.value)}
+            />
           </div>
 
           <div className="input-group">
             <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
+              <input
+                type="number"
+                name="latitude"
+                id="latitude"
+                required
+                value={latitude}
+                onChange={e => setLatitude(e.target.value)}
+              />
             </div>
 
             <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
+              <input
+                type="number"
+                name="longitude"
+                id="longitude"
+                required
+                value={longitude}
+                onChange={e => setLongitude(e.target.value)}
+              />
             </div>
           </div>
 
@@ -46,78 +125,22 @@ function App() {
 
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars0.githubusercontent.com/u/300170?s=400&v=4"
-                alt="Rogério Gomes Rios"
-              />
-              <div className="user-info">
-                <strong>Rogério Gomes Rios</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
+          {devs.map(dev => (
+            <li key={dev._id} className="dev-item">
+              <header>
+                <img src={dev.avatar_url} alt={dev.name} />
+                <div className="user-info">
+                  <strong>{dev.name}</strong>
+                  <span>{dev.techs.join(", ")}</span>
+                </div>
+              </header>
 
-            <p>
-              CTO na @RogerLista. Apaixonado pelas melhores tecnologias de
-              desenvolvimento
-            </p>
-            <a href="http://github.com/rogerlista">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars0.githubusercontent.com/u/300170?s=400&v=4"
-                alt="Rogério Gomes Rios"
-              />
-              <div className="user-info">
-                <strong>Rogério Gomes Rios</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-
-            <p>
-              CTO na @RogerLista. Apaixonado pelas melhores tecnologias de
-              desenvolvimento
-            </p>
-            <a href="http://github.com/rogerlista">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars0.githubusercontent.com/u/300170?s=400&v=4"
-                alt="Rogério Gomes Rios"
-              />
-              <div className="user-info">
-                <strong>Rogério Gomes Rios</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-
-            <p>
-              CTO na @RogerLista. Apaixonado pelas melhores tecnologias de
-              desenvolvimento
-            </p>
-            <a href="http://github.com/rogerlista">Acessar perfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars0.githubusercontent.com/u/300170?s=400&v=4"
-                alt="Rogério Gomes Rios"
-              />
-              <div className="user-info">
-                <strong>Rogério Gomes Rios</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-
-            <p>
-              CTO na @RogerLista. Apaixonado pelas melhores tecnologias de
-              desenvolvimento
-            </p>
-            <a href="http://github.com/rogerlista">Acessar perfil no Github</a>
-          </li>
+              <p>{dev.bio}</p>
+              <a href={`http://github.com/${dev.github_username}`}>
+                Acessar perfil no Github
+              </a>
+            </li>
+          ))}
         </ul>
       </main>
     </div>
